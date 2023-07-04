78ce5ad7c7ec073babe18ad69f80e28e4e7d08cf
Gojo9327 <138525650+Gojo9327@users.noreply.github.com>
2023-07-04 21:01:07 +0300

    Update config.js
    Signed-off-by: Gojo9327 <138525650+Gojo9327@users.noreply.github.com>

--- config.js
+++ config.js
@@ -37,13 +37,13 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
-global.ownername = 'DRIPS'
+global.owner = ['992929094253','']
+global.premium = ['992929094253','']
+global.botname = 'Bobo BOT'
+global.ownername = 'Aws'
 global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
 global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
-global.packname = 'ZIM BOT INC'
+global.packname = 'Bobo BOT'
 global.ytchannel = 'https://youtu.be/ww4z2m3uORU'
 global.monayawal = '100000'
 global.darahawal = '100'
